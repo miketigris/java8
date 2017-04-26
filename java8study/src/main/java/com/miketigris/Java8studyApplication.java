7165d0aaf6355128955b063f87dc281af862eec8
xuguirong@ppd.com <xuguirong@ppdai.com>
2017-04-26 23:05:07 +0800

    java8学习项目构建

---
+++ java8study/src/main/java/com/miketigris/Java8studyApplication.java
@@ -0,0 +1,12 @@
+package com.miketigris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8studyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8studyApplication.class, args);
+	}
+}
